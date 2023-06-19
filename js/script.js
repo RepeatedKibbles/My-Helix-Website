8109a4c143e3de17d136ce6f11bb2be2907dcdae
Jack D. Morgan <101306285+RepeatedKibbles@users.noreply.github.com>
2023-06-19 18:39:38 -0400

    Remake due to broken project code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-my-helix-website","displayName":"My Helix Website","link":{"linkType":"LinkTypePage","href":"#!page-my-helix-website"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
